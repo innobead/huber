a443fbb44bbbcde5e401707fc8b9cd4cfa021c21
David Ko <dko@suse.com>
2020-11-09 14:09:51 +0800

    Fix k3ai artifact path

--- src/generator/src/pkg/k3ai.rs
+++ src/generator/src/pkg/k3ai.rs
@@ -10,7 +10,7 @@ pub fn release() -> Package {
         },
         detail: None,
         targets: vec![PackageTargetType::LinuxAmd64(PackageManagement {
-            artifact_templates: vec!["{version}/k3sup".to_string()],
+            artifact_templates: vec!["{version}/install".to_string()],
             executable_templates: None,
             executable_mappings: Some(hashmap! {
                 "install".to_string() => "k3ai-install".to_string(),
