898d2cb574fe9e96585678863f9a53ece81b1e86
David Ko <dko@suse.com>
2021-02-19 05:11:28 +0800

    Fix windows build

--- hack/windows/release.ps1
+++ hack/windows/release.ps1
@@ -1,3 +1,3 @@
 cargo build --release --workspace --exclude=huber-generator
-Copy-Item ./target/release/huber.exe ./target/huber-windows-amd64.exe
+Rename-Item ./target/release/huber.exe huber-windows-amd64.exe
 ./hack/windows/generate-checksum.ps1 ./target/release
