f2dc262b7a8919803a574a393c4dd770f9a1b041
davidko <dko@suse.com>
2025-01-12 15:02:30 +0800

    fix: remove duplicated code

--- huber/tests/common/mod.rs
+++ huber/tests/common/mod.rs
@@ -1,10 +1,7 @@
 #![allow(dead_code)]
 #![allow(unused_macros)]
 
-use std::path::Path;
-
 use assert_cmd::assert::Assert;
-use assert_cmd::Command;
 
 pub const HUBER_EXEC: &str = env!("CARGO_BIN_EXE_huber");
 pub const INVALID_PKG: &str = "pkg_notfound";
@@ -14,106 +11,17 @@ pub const PKG_VERSION_1_ASSERT: &str = "k9s@v0.32.5";
 pub const PKG_VERSION_2: &str = "k9s@0.32.7";
 pub const PKG_VERSION_2_ASSERT: &str = "k9s@v0.32.7";
 
-pub fn install_pkg(name: &str) -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("install")
-        .arg(name)
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-pub fn uninstall_pkg(name: &str) -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("uninstall")
-        .arg(name)
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-pub fn lock_pkg(name_version: &str) -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("lock")
-        .arg(name_version)
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-pub fn update_pkg(name: &str) -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("update")
-        .arg(name)
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-pub fn save_pkg_list(file: &str) -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("save")
-        .arg("--file")
-        .arg(file)
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-pub fn reset_huber() -> Assert {
-    Command::new(HUBER_EXEC)
-        .arg("reset")
-        .env(
-            "huber_pkg_root_dir",
-            Path::new(env!("CARGO_MANIFEST_DIR"))
-                .parent()
-                .unwrap()
-                .join("generated"),
-        )
-        .assert()
-        .success()
-}
-
-macro_rules! assert_eq_last_line {
-    ($value:expr, $expect:expr) => {
-        let line = String::from_utf8($value.clone())
-            .unwrap()
-            .lines()
-            .last()
-            .unwrap()
-            .to_string();
-
-        assert_eq!(line, $expect)
+macro_rules! huber_cmd {
+    ($($body:tt)*) => {
+        assert_cmd::Command::new(crate::common::HUBER_EXEC)
+            .env(
+                "huber_pkg_root_dir",
+                std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
+                    .parent()
+                    .unwrap()
+                    .join("generated"),
+            )
+            .$($body)*
     };
 }
 
@@ -130,16 +38,39 @@ macro_rules! assert_eq_last_line_regex {
     };
 }
 
-macro_rules! huber_cmd {
-    ($($body:tt)*) => {
-        assert_cmd::Command::new(crate::common::HUBER_EXEC)
-            .env(
-                "huber_pkg_root_dir",
-                std::path::Path::new(env!("CARGO_MANIFEST_DIR"))
-                    .parent()
-                    .unwrap()
-                    .join("generated"),
-            )
-            .$($body)*
+macro_rules! assert_eq_last_line {
+    ($value:expr, $expect:expr) => {
+        let line = String::from_utf8($value.clone())
+            .unwrap()
+            .lines()
+            .last()
+            .unwrap()
+            .to_string();
+
+        assert_eq!(line, $expect)
     };
+}
+
+pub fn install_pkg(name: &str) -> Assert {
+    huber_cmd!(arg("install").arg(name).assert().success())
+}
+
+pub fn uninstall_pkg(name: &str) -> Assert {
+    huber_cmd!(arg("uninstall").arg(name).assert().success())
+}
+
+pub fn lock_pkg(name_version: &str) -> Assert {
+    huber_cmd!(arg("lock").arg(name_version).assert().success())
+}
+
+pub fn update_pkg(name: &str) -> Assert {
+    huber_cmd!(arg("update").arg(name).assert().success())
+}
+
+pub fn save_pkg_list(file: &str) -> Assert {
+    huber_cmd!(arg("save").arg("--file").arg(file).assert().success())
+}
+
+pub fn reset_huber() -> Assert {
+    huber_cmd!(arg("reset").assert().success())
 }
