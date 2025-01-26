2def4a5715bab482b9bc17d4c54473b10168dc81
davidko <dko@suse.com>
2025-01-27 01:07:18 +0800

    ci: update setup-dev for windows

--- hack/windows/setup-dev.ps1
+++ hack/windows/setup-dev.ps1
@@ -7,6 +7,11 @@ function install_rust_dependencies {
     .\rustup-init.exe
     cargo version
   }
+
+  rustup component add rustfmt clippy
+  cargo install default-target
+  cargo install --git https://github.com/DevinR528/cargo-sort.git --tag v1.1.0 cargo-sort
+  cargo install cargo-udeps
 }
 
 install_rust_dependencies
